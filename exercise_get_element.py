58d1a4ea8d6493a59d775008023975ddf96b9e39
MarcosJG123 <mpjuarezgoni@mail.austral.edu.ar>
2026-05-20 17:42:32 -0300

    Update exercise_get_element.py

--- exercise_get_element.py
+++ exercise_get_element.py
@@ -12,4 +12,9 @@ def get_element(lista, indice):
     Returns:
         El elemento en la posición indicada o None si está fuera de rango
     """
-    pass  # Reemplazar con tu implementación
+    
+    if indice >= 0 and indice < len(lista):
+        return indice
+    else:
+        return "None"
+    pass 
